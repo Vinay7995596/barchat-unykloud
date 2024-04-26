982b79ead954d8b189b89f59d2854377075499d1
Vinay7995596 <ragichattuvinay7995@gmail.com>
2024-04-26 08:03:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import Stuckbargraph from "./components/barchat";
+
+const App = () => {
+  return(
+    <div>
+      <Stuckbargraph/>
     </div>
-  );
+  )
 }
-
 export default App;
